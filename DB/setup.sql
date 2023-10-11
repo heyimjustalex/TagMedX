adba23331e01f969cff68ef7155b80f3c8b247aa
Alex <202300831@post.au.dk>
2023-10-12 00:37:42 +0200

    Update db and make initial backend conf

--- DB/setup.sql
+++ DB/setup.sql
@@ -71,7 +71,7 @@ CREATE TABLE `Label` (
 CREATE TABLE `BBox` (
   id INT NOT NULL AUTO_INCREMENT,
   id_examination INT NOT NULL,
-  label_id INT,
+  id_label INT NOT NULL,
   comment VARCHAR(255),
   x1 FLOAT,
   y1 FLOAT,
@@ -79,5 +79,76 @@ CREATE TABLE `BBox` (
   y2 FLOAT,
   PRIMARY KEY (id),
   FOREIGN KEY (id_examination) REFERENCES `Examination`(id),
-  FOREIGN KEY (label_id) REFERENCES `Label`(id)
+  FOREIGN KEY (id_label) REFERENCES `Label`(id)
 );
+
+
+INSERT INTO `User` (e_mail, password_hash, name, surname, title, role, description, experience)
+VALUES
+    ('user1@example.com', 'hash1', 'John', 'Doe', 'Mr.', 'User', 'Description 1', 'Experience 1'),
+    ('user2@example.com', 'hash2', 'Jane', 'Smith', 'Ms.', 'User', 'Description 2', 'Experience 2'),
+    ('user3@example.com', 'hash3', 'Alice', 'Johnson', 'Dr.', 'Admin', 'Description 3', 'Experience 3'),
+    ('user4@example.com', 'hash4', 'Bob', 'Wilson', 'Mr.', 'User', 'Description 4', 'Experience 4'),
+    ('user5@example.com', 'hash5', 'Eve', 'Davis', 'Ms.', 'User', 'Description 5', 'Experience 5');
+
+
+INSERT INTO `Group` (name, description)
+VALUES
+    ('Group 1', 'Description 1'),
+    ('Group 2', 'Description 2'),
+    ('Group 3', 'Description 3'),
+    ('Group 4', 'Description 4'),
+    ('Group 5', 'Description 5');
+
+
+INSERT INTO `Task` (id_group, max_samples_for_user, name, description, type)
+VALUES
+    (1, 1, 'Task 1', 'Task Description 1', 'Type A'),
+    (2, 2, 'Task 2', 'Task Description 2', 'Type B'),
+    (3, 2, 'Task 3', 'Task Description 3', 'Type A'),
+    (4, 3, 'Task 4', 'Task Description 4', 'Type C'),
+    (5, 4, 'Task 5', 'Task Description 5', 'Type B');
+
+
+INSERT INTO `Membership` (id_user, id_group)
+VALUES
+    (1, 1),
+    (2, 1),
+    (2, 2),
+    (3, 2),
+    (4, 3);
+
+
+INSERT INTO `Sample` (id_task, path, format)
+VALUES
+    (1, '/path/to/sample1', 'Format A'),
+    (2, '/path/to/sample2', 'Format B'),
+    (3, '/path/to/sample3', 'Format A'),
+    (4, '/path/to/sample4', 'Format C'),
+    (2, '/path/to/sample5', 'Format B');
+
+
+INSERT INTO `Examination` (id_user, id_sample, to_further_verification, bad_quality)
+VALUES
+    (1, 1, 0, 0),
+    (2, 2, 1, 0),
+    (3, 3, 0, 1),
+    (4, 4, 1, 0),
+    (2, 5, 0, 0);
+
+INSERT INTO `Label` (id_task, name, description)
+VALUES
+    (1, 'Label 1', 'Label Description 1'),
+    (2, 'Label 2', 'Label Description 2'),
+    (3, 'Label 3', 'Label Description 3'),
+    (4, 'Label 4', 'Label Description 4'),
+    (2, 'Label 5', 'Label Description 5');
+
+
+INSERT INTO `BBox` (id_examination, id_label, comment, x1, y1, x2, y2)
+VALUES
+    (1, 1, 'Comment 1', 10.0, 20.0, 30.0, 40.0),
+    (2, 2, 'Comment 2', 15.0, 25.0, 35.0, 45.0),
+    (3, 3, 'Comment 3', 20.0, 30.0, 40.0, 50.0),
+    (4, 4, 'Comment 4', 25.0, 35.0, 45.0, 55.0),
+    (2, 5, 'Comment 5', 30.0, 40.0, 50.0, 60.0);
---
+++ backend/models/models.py
@@ -0,0 +1,137 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'User'
+
+    id = Column(Integer, primary_key=True)
+    e_mail = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
+    name = Column(String)
+    surname = Column(String)
+    title = Column(String)
+    role = Column(String, nullable=False)
+    description = Column(String)
+    experience = Column(String)
+    
+    # Define the one-to-many relationship with Membership
+    Membership = relationship("Membership", back_populates="User")
+
+    # Define the one-to-many relationship with Examination
+    Examination = relationship("Examination", back_populates="User")
+
+    
+class Membership(Base):
+    __tablename__ = 'Membership'
+
+    id_user = Column(Integer, ForeignKey('User.id'), primary_key=True)
+    id_group = Column(Integer, ForeignKey('Group.id'), primary_key=True)
+
+    # Define the many-to-one relationship with User
+    User = relationship("User", back_populates="Membership")
+
+    # Define the many-to-one relationship with Group
+    Group = relationship("Group", back_populates="Membership")
+
+class Group(Base):
+    __tablename__ = 'Group'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+    # Define the one-to-many relationship with Task
+    Task = relationship("Task", back_populates="Group")
+
+    # Define the one-to-many relationship with Group
+    Membership = relationship("Membership", back_populates="Group")
+
+
+class Task(Base):
+    __tablename__ = 'Task'
+
+    id = Column(Integer, primary_key=True)
+    id_group = Column(Integer, ForeignKey('Group.id'), nullable=False)
+    max_samples_for_user = Column(Integer)
+    name = Column(String)
+    description = Column(String)
+    type = Column(String)
+
+    # Define the many-to-one relationship with Group
+    Group = relationship("Group", back_populates="Task")
+
+    # Define the one-to-many relationship with Sample
+    Sample = relationship("Sample", back_populates="Task")
+
+    # Define the one-to-many relationship with Label
+    Label = relationship("Label", back_populates="Task")
+
+
+class Sample(Base):
+    __tablename__ = 'Sample'
+
+    id = Column(Integer, primary_key=True)
+    id_task = Column(Integer, ForeignKey('Task.id'), nullable=False)
+    path = Column(String)
+    format = Column(String)
+
+    # Define the many-to-one relationship with Task
+    Task = relationship("Task", back_populates="Sample")
+
+    # Define the one-to-many relationship with Examination
+    Examination = relationship("Examination", back_populates="Sample")
+
+class Examination(Base):
+    __tablename__ = 'Examination'
+
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('User.id'), nullable=False)
+    id_sample = Column(Integer, ForeignKey('Sample.id'), nullable=False)
+    to_further_verification = Column(Boolean)
+    bad_quality = Column(Boolean)
+
+    # Define the many-to-one relationship with User
+    User = relationship("User", back_populates="Examination")
+
+    # Define the many-to-one relationship with Sample
+    Sample = relationship("Sample", back_populates="Examination")
+
+    # Define the one-to-many relationship with BBox
+    BBox = relationship("BBox", back_populates="Examination")
+
+
+
+class Label(Base):
+    __tablename__ = 'Label'
+
+    id = Column(Integer, primary_key=True)
+    id_task = Column(Integer, ForeignKey('Task.id'), nullable=False)
+    name = Column(String)
+    description = Column(String)
+
+    # Define the many-to-one relationship with Task
+    Task = relationship("Task", back_populates="Label")
+
+    # Define the one-to-many relationship with BBox
+    BBox = relationship("BBox", back_populates="Label")
+
+class BBox(Base):
+    __tablename__ = 'BBox'
+
+    id = Column(Integer, primary_key=True)
+    id_examination = Column(Integer, ForeignKey('Examination.id'), nullable=False)
+    id_label = Column(Integer, ForeignKey('Label.id'), nullable=False)
+    comment = Column(String)
+    x1 = Column(Float)
+    y1 = Column(Float)
+    x2 = Column(Float)
+    y2 = Column(Float)
+
+    # Define the many-to-one relationship with Examination
+    Examination = relationship("Examination", back_populates="BBox")
+
+    # Define the many-to-one relationship with Label
+    Label = relationship("Label", back_populates="BBox")
