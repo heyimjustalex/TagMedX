6f0f011350004c276445868e4d213811a5161ca2
Michal Pawlowski <michal.pawlowski@ibm.com>
2023-11-04 11:21:22 +0100

    feature: make groups table tools rwd

--- frontend/src/components/GroupsTable/GroupsTableTopContent.jsx
+++ frontend/src/components/GroupsTable/GroupsTableTopContent.jsx
@@ -6,50 +6,53 @@ import {
   DropdownTrigger,
   Input
 } from '@nextui-org/react';
-import { IconChevronDown, IconPlus, IconSearch } from '@tabler/icons-react';
+import { IconChevronDown, IconPlus, IconSearch, IconUsersGroup } from '@tabler/icons-react';
 
 import { roleOptions } from './GroupsTableConsts';
 
 export default function GroupsTableTopContent({ filterValue, onClear, onSearchChange, statusFilter, setStatusFilter }) {
   return (
     <div className='flex flex-col gap-4'>
-        <div className='flex justify-between gap-3 items-end'>
-          <Input
-            isClearable
-            className='w-full sm:max-w-full'
-            placeholder='Search by name...'
-            startContent={<IconSearch />}
-            value={filterValue}
-            onClear={() => onClear()}
-            onValueChange={onSearchChange}
-          />
-          <div className='flex gap-3'>
-            <Dropdown>
-              <DropdownTrigger className='hidden sm:flex'>
-                <Button endContent={<IconChevronDown className='text-small' />} variant='flat'>
-                  Status
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                disallowEmptySelection
-                aria-label='Table Columns'
-                closeOnSelect={false}
-                selectedKeys={statusFilter}
-                selectionMode='multiple'
-                onSelectionChange={setStatusFilter}
-              >
-                {roleOptions.map((status) => (
-                  <DropdownItem key={status.uid} className='capitalize'>
-                    {status.name}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-            <Button color='primary' endContent={<IconPlus />}>
-              Add New
-            </Button>
-          </div>
+      <div className='flex flex-col sm:flex-row justify-between gap-3 items-end'>
+        <Input
+          isClearable
+          className='w-full sm:max-w-full'
+          placeholder='Search by name...'
+          startContent={<IconSearch />}
+          value={filterValue}
+          onClear={() => onClear()}
+          onValueChange={onSearchChange}
+        />
+        <div className='flex w-full sm:w-min gap-3'>
+          <Dropdown>
+            <DropdownTrigger className='flex w-full sm:w-min justify-end sm:justify-end'>
+              <Button endContent={<IconChevronDown className='text-small' />} variant='flat'>
+                Status
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              disallowEmptySelection
+              aria-label='Table Columns'
+              closeOnSelect={false}
+              selectedKeys={statusFilter}
+              selectionMode='multiple'
+              onSelectionChange={setStatusFilter}
+            >
+              {roleOptions.map((status) => (
+                <DropdownItem key={status.uid} className='capitalize'>
+                  {status.name}
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+          <Button className='min-w-fit' color='primary' variant='flat' endContent={<IconPlus />}>
+            Add New
+          </Button>
+          <Button className='min-w-fit' color='primary' endContent={<IconUsersGroup />}>
+            Join Group
+          </Button>
         </div>
       </div>
+    </div>
   )
 }
