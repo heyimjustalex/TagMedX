2d5f04961dd159e7871865d48d3f3fa696125fb5
Michal Pawlowski <michal.pawlowski@ibm.com>
2023-11-29 13:43:00 +0100

    feature: display packages only from selected set

--- frontend/src/components/Group/GroupPackages/GroupPackages.jsx
+++ frontend/src/components/Group/GroupPackages/GroupPackages.jsx
@@ -13,7 +13,8 @@ export default function GroupPackages({ data, setData }) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const notification = useNotification();
-  const [tab, setId] = [searchParams.get('tab'), searchParams.get('set')];
+  const setId = searchParams.get('set');
+  const setIdNum = parseInt(setId)
   const selectedKey = setId ? new Set().add(setId) : new Set();
   const [packages, setPackages] = useState(data.packages);
 
@@ -33,7 +34,7 @@ export default function GroupPackages({ data, setData }) {
           className='h-10 w-full'
           placeholder='Select set to display packages'
           classNames={{ inputWrapper: 'h-10', trigger: 'h-10 min-h-10' }}
-          onChange={e => router.push(`/groups/${data.id}?tab=${tab}&set=${e.target.value}`)}
+          onChange={e => router.push(`/groups/${data.id}?tab=packages&set=${e.target.value}`)}
         >
           {data.sets.map((set) => (
             <SelectItem key={set.id} value={set.user_id}>
@@ -62,7 +63,11 @@ export default function GroupPackages({ data, setData }) {
       {
         packages?.length > 0
         ? <div className='flex flex-row flex-wrap gap-4 w-full mt-4'>
-          {packages?.map(e => <GroupPackage key={`${e.id}-${e.id_user}`} data={e} users={data.users} setPackages={setPackages} />)}
+          {
+            packages
+            .filter(e => e.id_set === setIdNum)
+            .map(e => <GroupPackage key={`${e.id}-${e.id_user}`} data={e} users={data.users} setPackages={setPackages} /> )
+          }
         </div>
         : <div className='flex text-foreground-400 items-center justify-center h-40'>
           {data.role === 'Admin' && data?.sets?.length === 0 ? 'No packages found, create set first.' : 'No packages found.'}
