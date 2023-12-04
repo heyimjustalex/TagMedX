1897bf379ecbb46affd3908deacc1b43555f9cb2
Oskar Fiedot <oskar.fiedot@gmail.com>
2023-12-04 18:25:09 +0100

    feature: add examination repository and service

---
+++ backend/features/examination/services/examination_service.py
@@ -0,0 +1,61 @@
+from sqlalchemy.orm import Session
+from repositories.examination_repository import ExaminationRepository
+from models.models import Examination
+from fastapi import HTTPException, status
+
+
+class ExaminationService:
+    def __init__(self, db: Session):
+        self.repository = ExaminationRepository(db)
+
+    def create_examination(
+        self,
+        id_user: int,
+        id_sample: int,
+        to_further_verification: bool | None = None,
+        bad_quality: bool | None = None,
+    ) -> Examination:
+        examination = Examination()
+
+        examination.id_user == id_user
+        examination.id_sample == id_sample
+        examination.to_further_verification == to_further_verification
+        examination.bad_quality == bad_quality
+
+        self.repository.create_examination(examination)
+        return examination
+
+    def update_examination(
+        self,
+        id_examination: int,
+        to_further_verification: bool | None = None,
+        bad_quality: bool | None = None,
+    ) -> Examination:
+        examination = self.get_examination(id_examination)
+
+        if to_further_verification:
+            examination.to_further_verification = to_further_verification
+
+        if bad_quality:
+            examination.bad_quality = bad_quality
+
+        self.repository.update()
+        return examination
+
+    def delete_examination(self, id_examination: int):
+        examination = self.get_examination(id_examination)
+        self.repository.delete_examination(examination)
+
+    def get_examination(self, id_examination: int) -> Examination:
+        examination = self.repository.get_examination_by_id(id_examination)
+        if not examination:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Examination not found"
+            )
+        return examination
+
+    def get_user_examinations(self, id_user: int) -> list[Examination]:
+        return self.repository.get_examinations_by_user(id_user)
+
+    def get_sample_examinations(self, id_sample: int) -> list[Examination]:
+        return self.repository.get_examinations_by_sample(id_sample)
